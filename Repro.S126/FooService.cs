5e5412b0be2b386174ef5b3764bf9ba9d4e97aef
Sandro Ciervo <sc@neolution.ch>
2020-12-10 12:24:28 +0100

    Add files to reproduce

---
+++ Repro.S126/FooService.cs
@@ -0,0 +1,34 @@
+﻿namespace Repro.S126
+{
+    public class FooService
+    {
+        public string Foo(string name)
+        {
+            if (name == null)
+            {
+                return "Foo";
+            }
+            else if (name == string.Empty)
+            {
+                return "Bar";
+            }
+
+            return "FooBar";
+        }
+
+        public string FooWithoutRedundantElse(string name)
+        {
+            if (name == null)
+            {
+                return "Foo";
+            }
+
+            if (name == string.Empty)
+            {
+                return "Bar";
+            }
+
+            return "FooBar";
+        }
+    }
+}
